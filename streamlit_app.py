35966eb1a7872308b1d514148b7a1a9b75d17317
allensel <145963327+allensel@users.noreply.github.com>
2023-10-08 13:40:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
+# my_data_rows = my_cur.fetchall()
 streamlit.header("the fruit load list contains:")
 # streamlit.dataframe(my_data_rows)
 my_cur.execute("Insert into fruit_load_list values('allen')")
